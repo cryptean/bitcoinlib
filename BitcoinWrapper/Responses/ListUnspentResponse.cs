a7fe75e58fafc793bcd20ce6d7e1c1f3132fdf09
George Kimionis <mcaizgk2@gmail.com>
2014-01-21 03:32:21 +0200

    Added missing field Account in ListUnspentResponse

--- BitcoinWrapper/Responses/ListUnspentResponse.cs
+++ BitcoinWrapper/Responses/ListUnspentResponse.cs
@@ -10,6 +10,7 @@ namespace BitcoinLib.Responses
         public String TxId { get; set; }
         public Int32 VOut { get; set; }
         public String Address { get; set; }
+        public String Account { get; set; }
         public String ScriptPubKey { get; set; }
         public Decimal Amount { get; set; }
         public Int32 Confirmations { get; set; }
