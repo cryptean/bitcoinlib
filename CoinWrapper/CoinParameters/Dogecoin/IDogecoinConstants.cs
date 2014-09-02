840209d121d5cd6b87276ce01ff782b78e140004
Shaun Coffee <lecoffeestain@gmail.com>
2014-09-02 21:15:30 +0100

    Adding DogeCoin :D

---
+++ CoinWrapper/CoinParameters/Dogecoin/IDogecoinConstants.cs
@@ -0,0 +1,10 @@
+// Copyright (c) 2014 George Kimionis & Shaun Barratt
+// Distributed under the GPLv3 software license, see the accompanying file LICENSE or http://opensource.org/licenses/GPL-3.0
+
+namespace BitcoinLib.CoinParameters.Dogecoin
+{
+	public interface IDogecoinConstants
+	{
+		DogecoinConstants.Constants Constants { get; }
+	}
+}
---
+++ CoinWrapper/Services/Coins/Dogecoin/DogecoinService.cs
@@ -0,0 +1,27 @@
+// Copyright (c) 2014 George Kimionis & Shaun Barratt
+// Distributed under the GPLv3 software license, see the accompanying file LICENSE or http://opensource.org/licenses/GPL-3.0
+
+using System;
+using BitcoinLib.CoinParameters.Dogecoin;
+
+namespace BitcoinLib.Services.Coins.Dogecoin
+{
+	public class DogecoinService : CoinService, IDogecoinService
+	{
+		public DogecoinService(Boolean useTestnet = false) : base(useTestnet)
+		{
+		}
+
+		public DogecoinService(String daemonUrl, String rpcUsername, String rpcPassword, String walletPassword = null) : base(daemonUrl, rpcUsername, rpcPassword, walletPassword)
+		{
+		}
+
+		public DogecoinConstants.Constants Constants
+		{
+			get
+			{
+				return DogecoinConstants.Constants.Instance;
+			}
+		}
+	}
+}
