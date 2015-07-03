e62a6653f83cb1f2c986a86d561e789fa774d7fa
weitaolee <12666954@qq.com>
2015-07-03 18:43:39 +0800

    transaction response add fee field

--- CoinWrapper/Responses/GetTransactionResponse.cs
+++ CoinWrapper/Responses/GetTransactionResponse.cs
@@ -11,6 +11,7 @@ namespace BitcoinLib.Responses
     public class GetTransactionResponse : ITransactionResponse
     {
         public Decimal Amount { get; set; }
+        public Decimal Fee { get; set; }
         public String BlockHash { get; set; }
         public Int32 BlockIndex { get; set; }
         public Int32 BlockTime { get; set; }
@@ -28,6 +29,7 @@ namespace BitcoinLib.Responses
         public String Account { get; set; }
         public String Address { get; set; }
         public Decimal Amount { get; set; }
+        public Decimal Fee { get; set; }
         public String Category { get; set; }
     }
 }
