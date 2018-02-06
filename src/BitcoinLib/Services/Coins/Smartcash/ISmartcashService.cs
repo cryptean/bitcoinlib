c42d7053eff7c20dc4fc9c057285d353e2a498de
rc125 <cruzrenato@gmail.com>
2018-02-06 01:06:28 -0300

    SmartCash cryptocurrency support
    Extenden BitcoinLib with SmartCash support

---
+++ src/BitcoinLib/Services/Coins/Smartcash/ISmartcashService.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2014 - 2016 George Kimionis
+// See the accompanying file LICENSE for the Software License Aggrement
+
+using BitcoinLib.CoinParameters.Smartcash;
+using BitcoinLib.Services.Coins.Base;
+
+namespace BitcoinLib.Services.Coins.Smartcash
+{
+    public interface ISmartcashService : ICoinService, ISmartcashConstants
+    {
+    }
+}
