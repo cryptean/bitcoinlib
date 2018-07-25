1b7a242390554a9e26fee5cb0d6e72d311af49f9
Michael Allar <allar@michaelallar.com>
2018-07-25 07:21:16 -0700

    Move transactions do not have TxIDs

--- demo/Program.cs
+++ demo/Program.cs
@@ -126,6 +126,12 @@ namespace ConsoleClient
                     Console.WriteLine("\n\nMy transactions' details:");
                     foreach (var transaction in myTransactions)
                     {
+                        // Move transactions don't have a txId, which this logic fails for
+                        if (transaction.Category == "move")
+                        {
+                            continue;
+                        }
+                        
                         var localWalletTransaction = CoinService.GetTransaction(transaction.TxId);
                         IEnumerable<PropertyInfo> localWalletTrasactionProperties = localWalletTransaction.GetType().GetProperties();
                         IList<GetTransactionResponseDetails> localWalletTransactionDetailsList = localWalletTransaction.Details.ToList();
